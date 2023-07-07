1d097c6978b2e1112defc5e491dc0c4dcfa9a16c
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-07-07 04:30:31 -0300

    commit inicial - estrutura de rotas

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import SignUp from "./components/signup";
+import Login from "./components/login";
+import CashIn from "./components/cashin";
+import CashOut from "./components/cashout";
+import UserContext from "./components/usercontext";
+
+function App() {
+    const [userData, setUserData] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ userData, setUserData }}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />}></Route>
+                    <Route path="/cadastro" element={<SignUp />}></Route>
+                    <Route path="/home" element={<Home />}></Route>
+                    <Route
+                        path="/nova-transacao/entrada"
+                        element={<CashIn />}
+                    ></Route>
+                    <Route
+                        path="/nova-transacao/saida"
+                        element={<CashOut />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
+    );
+}
+
+export default App;
